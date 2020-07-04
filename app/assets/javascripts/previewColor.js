75e30e3f18c2d90434e5181a0ee9412730e78985
hiiihaaa <snshiiihaaa@gmail.com>
2020-07-04 12:19:43 +0900

    fix

--- app/assets/javascripts/previewColor.js
+++ app/assets/javascripts/previewColor.js
@@ -1,15 +1,18 @@
 $(document).on('turbolinks:load', function(){
   $('.box').on('click', function(){
-    var targetColor = $(this).data('color')
+    var targetColor = document.getElementById(this.id).style.getPropertyValue('background-color')
     var targetOpacity = document.getElementById(this.id).style.opacity
-    $('.preview').attr('data-color', targetColor)
-    $('.preview').css('background-color', '#'+targetColor)
-    $('.preview').attr('data2-opacity', targetOpacity)
-    $('.preview').css('opacity', targetOpacity)
-    $(".show-value__hexadecimal--value").empty()
-    $(".show-value__hexadecimal--value").append("<div class='show-value__hexadecimal__text'>#" + targetColor + "</div>")
-    $(".show-value__opacity--value").empty()
-    $(".show-value__opacity--value").append("<div class='show-value__opacity__text'>"+targetOpacity+"</div>")
-    $('#opacity_switch').attr('data-color', targetColor)
+    var color16 = $(this).data("color")
+    if($('#color-target').get(0).value==1){
+      $('.preview').css('background-color', targetColor)
+      $('.preview').css('opacity', targetOpacity)
+      $(".show-value__hexadecimal--value").empty()
+      $(".show-value__hexadecimal--value").append("<div class='show-value__hexadecimal__text'>#" + color16 + "</div>")
+      $(".show-value__opacity--value").empty()
+      $(".show-value__opacity--value").append("<div class='show-value__opacity__text'>" + targetOpacity + "</div>")
+    }else if($('#color-target').get(0).value==2){
+      $('.preview-text__input').css('color', targetColor)
+      $('.preview-text__input').css('opacity', targetOpacity)
+    }
   })
 })
